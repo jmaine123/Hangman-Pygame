0ae22f3b775bb1386e4d11740942690bf4999643
Jermaine Smikle <jermaine.smikle@aol.com>
2024-04-07 19:31:59 -0400

    added category page

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame, sys, random
 
 FPS = 60
 pygame.init()
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
 PURPLE = (51, 0, 204)
 BLACK = (0, 0, 0)
@@ -30,7 +30,30 @@ guessed_letters = set()
 
 
 # Load word list (you can replace this with your own list)
-word_list = [ "The Hulk", "Spiderman", "The Avengers", "Iron Man", "Captain America",
+# word_list = [ "The Hulk", "Spiderman", "The Avengers", "Iron Man", "Captain America",
+#         "Thor", "Loki", "Black Widow", "Hawk Eye", "The Red Skull", "Thanos",
+#         "Scarlett Witch", "Vison", "Zemo", "Green Goblin", "The Jackal",
+#         "Fantastic Four", "The Thing", "Silver Surfer",
+#         "Guardians of the Galaxy", "Nick Fury", "Shield", "Vulture",
+#         "Doctor Octopus", "Venom", "The Punisher", "Blade", "Magneto",
+#         "Wolverine", "Cyclops", "Jean Grey", "Beast", "Iron Fist", "Luke Cage",
+#         "Dare Devil", "Scorpion", "Black Panther", "Wakanda",
+#         "Stark Enterprise", "Tony Stark", "Power man", "Abomination",
+#         "Carnage", "I am Groot", "Avengers Assemble", "War Machine",
+#         "Mister Fantasic", "Invisible Woman", "The Human Torch", "Venom",
+#         "Scarlet Witch", "The Fantastic Four", "The Mandarin", "The Wasp",
+#         "Wolverine", "Cyclops", "Jean Grey", "Beast", "Luke Cage",
+#         "Dare Devil", "Sand man", "Beast", "Silver Surfer", "Captain Marvel",
+#         "Venom", "Magneto", "Night Crawler", "Madame Web", "Carnage",
+#         "King pin", "Doctor Octopus", "The Lizard", " She Hulk"
+#         ]
+
+categories = ["Marvel", "NBA", "Artists"]
+
+
+word_dict = {
+    "Marvel": [
+        "The Hulk", "Spiderman", "The Avengers", "Iron Man", "Captain America",
         "Thor", "Loki", "Black Widow", "Hawk Eye", "The Red Skull", "Thanos",
         "Scarlett Witch", "Vison", "Zemo", "Green Goblin", "The Jackal",
         "Fantastic Four", "The Thing", "Silver Surfer",
@@ -46,7 +69,32 @@ word_list = [ "The Hulk", "Spiderman", "The Avengers", "Iron Man", "Captain Amer
         "Dare Devil", "Sand man", "Beast", "Silver Surfer", "Captain Marvel",
         "Venom", "Magneto", "Night Crawler", "Madame Web", "Carnage",
         "King pin", "Doctor Octopus", "The Lizard", " She Hulk"
-        ]
+    ],
+    "Artists": [
+        "Jay Z", "Queen Latifah", "DMX", "Snoop Dogg", "Jadakiss", "J Cole",
+        "Jermaine Dupri", "Alicia Keys", "John Legend", "Lil Jon", "Lil Kim",
+        "Diddy", "Tupac", "Lil Wayne", "Beastie Boyz", "Ying Yang Twins",
+        "Drake", "Cash Money", "Ruff Ryders", "Bad Boy Entertainment",
+        "Nicki Minaj", "Bryson Tiller", "Torey Lanez", "Summer Walker",
+        "Usher", "Cardi B", "DMX", "Meg The Stallion", "Chris Brown",
+        "Rihanna", "The Fugees", "Aretha Franklin", "Jay Rock", "Kanye West",
+        "Kendrick Lamar", "Kid Cudi", "James Brown", "Michael Jackson"
+    ],
+    "NBA": [
+        "LeBron James", "Carmelo Anthony", "Michael Jordan", "Kobe Bryant",
+        "Tim Duncan", "Kevin Garnet", "Russell Westbrook", "Pick and Roll",
+        "New York Knicks", "Los angeles Lakers", " Los Angeles Clippers",
+        "New Orlean Pelicans", "Charlotte Hornets", "Golden State Warriors",
+        "Sacremento Kings", "Toronto Raptors", "Stephen Curry", "Kevin Durant",
+        "Kyrie Irving", "Kawhi Leonard", "Boston Celtics", "Miami Heat",
+        "Magic Johnson", "Houston Rockets", "Chicago Bulls",
+        "Oklahoma City Thunder", "Denver Nuggets", "Dallas Mavericks",
+        "Portland Trail Blazers", "Brooklyn Nets", "Jayson Tatum",
+        "Jaylen Brown", "Anthony Davis", "Jimmy Butler", "James Harden",
+        "Jimmy Butler", "Shaquille O'Neal", "Kyrie Irving", "Zion Williamson",
+        "Ja Morant"
+    ]
+}
 # chosen_word = random.choice(word_list).upper()
 
 
@@ -64,7 +112,7 @@ class Button:
     def draw(self):
         # menu_font = pygame.font.Font('freesansbold.ttf', 32)
         text = menu_font.render(self.text, True, BLACK)
-        pygame.draw.rect(screen, self.color, [self.pos[0], self.pos[1], 200, 50])
+        pygame.draw.rect(screen, self.color, self.button)
         screen.blit(text, ( self.pos[0] + 10, self.pos[1]+ 10))
     
     def checkClicked(self):
@@ -72,6 +120,11 @@ class Button:
         mouse_click = pygame.mouse.get_pressed()
         if self.button.collidepoint(mouse_pos) and mouse_click[0]:
             return True
+    
+    def isHoveredOver(self, color):
+        mouse_pos = pygame.mouse.get_pos()
+        over = self.button.collidepoint(mouse_pos)
+            
 
 
 def displayGameStatus(display_word, attempts_left, chosen_word):
@@ -98,8 +151,9 @@ def displayGameStatus(display_word, attempts_left, chosen_word):
         
         
 
-def configBoard():
-    pass
+def configBoard(category):
+    chosen_word = random.choice(word_dict[category]).upper()
+    return chosen_word
 
 
 def swapLetters(chosen_word):
@@ -114,6 +168,49 @@ def swapLetters(chosen_word):
     return "".join(board)
 
 
+def drawGuessLetters(chosen_word):
+    font = pygame.font.Font(None, FONT_SIZE)
+    guessed_msg = font.render("Guessed Letters: ", True, WHITE)
+    screen.blit(guessed_msg, (0, 550))
+    first_ltr_x = guessed_msg.get_width() + 10
+    for ltr in guessed_letters:
+        if ltr in chosen_word:
+            ltr_surface = font.render(ltr, True, GREEN)
+        else:
+            ltr_surface = font.render(ltr, True, RED)
+        screen.blit(ltr_surface, (first_ltr_x, 550))
+        
+        first_ltr_x+= 30
+        
+
+def selectCategory():
+    selected = False
+    
+    while selected == False:         
+        screen.fill(YELLOW)
+        firstbtn_x = 300
+        firstbtn_y = 100
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        marvel_btn = Button("Marvel", GREEN, (300, 100))
+        marvel_btn.draw()
+        nba_btn = Button("NBA", GREEN, (300, 200))
+        nba_btn.draw()
+        artists_btn = Button("Artists", GREEN, (300, 300))
+        artists_btn.draw()
+        
+        
+        pygame.display.update()
+            
+        
+        
+        
+        
+
+
 def mainMenu():
     
     running = True
@@ -122,8 +219,10 @@ def mainMenu():
         click = pygame.mouse.get_pressed()
         screen.fill(YELLOW)
         
-        # menu_font = pygame.font.Font('freesansbold.ttf', 32)
-        text = menu_font.render('New Game', True, BLACK)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
         menu_x = 300
         menu_y = SCREEN_HEIGHT//2
         new_game = Button("New Game", GREEN, (menu_x, menu_y))
@@ -134,22 +233,21 @@ def mainMenu():
         # menu_btn = pygame.draw.rect(screen, GREEN, [menu_x, menu_y, 200, 50])
         # screen.blit(text, (menu_x + 10, menu_y + 10))
         # start_game = False
+        pygame.display.flip()
         
         if new_game.checkClicked():
-            global guessed_letters 
+            selectCategory()
+            global guessed_letters
             guessed_letters = set()
-            chosen_word = random.choice(word_list).upper()  
+            chosen_word = configBoard("NBA") 
             attempts_left = 6
             playGame(chosen_word, attempts_left)
             pygame.time.delay(3000)
         if exit_btn.checkClicked():
+            running = False
             pygame.quit()
             
-        pygame.display.update()
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
             
     
 
@@ -165,6 +263,7 @@ def playGame(chosen_word, attempts_left):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
         
             
         screen.fill(PURPLE)
@@ -189,6 +288,7 @@ def playGame(chosen_word, attempts_left):
                     if letter not in chosen_word:
                         attempts_left -= 1
         
+        drawGuessLetters(chosen_word)
                         
         # Update the display
         pygame.display.flip()
@@ -198,9 +298,11 @@ def playGame(chosen_word, attempts_left):
         # Check for game over conditions
         if attempts_left == 0 or " __ " not in display_word:
             running = False
-    
+
+
 mainMenu()
 # playGame()
+# selectCategory()
 
 # Wait for a moment before quitting
 pygame.time.delay(2000)
