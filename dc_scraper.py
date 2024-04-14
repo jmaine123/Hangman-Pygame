23c14f359d041c1f933598c544756abce730b932
Jermaine Smikle <jermaine.smikle@aol.com>
2024-04-14 01:36:31 -0400

    Webscrape DC Comics and appended to main word dict

--- dc_scraper.py
+++ dc_scraper.py
@@ -14,14 +14,53 @@ import time
 from selenium.webdriver.common.by import By
 from dataclasses import dataclass, asdict
 from tqdm import tqdm
+import pickle
 
 
+dc_dict = {"DC Comics": []}
 
+def contains_number(string):
+    for i in list(string):
+        if i.isdigit():
+            return True
+    return False
 
-@dataclass
-class CharacterInfo:
-    name: str
     
-dc_dict = {"DC": []}
 
-print(dc_dict)
+class Scraper:
+    def getInfo(self):
+        
+        for i in range(1,14):
+            url = f'https://www.dc.com/characters?page={i}'
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content, "html.parser")
+            s = soup.prettify()
+            dom = etree.HTML(str(soup))
+            characters = dom.xpath('//div[contains(@class, "item-primary")]/a/@aria-label')
+
+            for character in characters:
+                
+                if contains_number(character) == False:
+                    dc_dict["DC Comics"].append(character)
+    
+    
+
+
+
+def main():
+    project = Scraper()
+    project.getInfo()
+    
+    print(dc_dict)
+
+    # opening file in write mode (binary) 
+    file = open("dc_characters.txt", "wb") 
+    
+    # serializing dictionary  
+    pickle.dump(dc_dict, file)
+    
+    # closing the file 
+    file.close()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, sys, random
 import os, sys, ast
-
+import pickle
 
 
 #Game Settings
@@ -58,10 +58,24 @@ for i in range(0,7):
     
 
 
-
+# Import words from external file that I manually added
 with open("/Users/user/Desktop/Code_Games/Hangman/words.py") as f:
     data = f.read()
     word_dict = ast.literal_eval(data)
+    
+
+# reading data from webscraping
+with open('dc_characters.txt', 'rb') as handle: 
+    data = handle.read() 
+  
+  
+# reconstructing the data as dictionary 
+dc_characters = pickle.loads(data)
+
+
+# combing data from multiple sources both manual and webscraping
+word_dict.update(dc_characters)
+
     
 # print(word_dict)
 category_list = word_dict.keys()
@@ -71,83 +85,6 @@ selected_cat = "Marvel"
 
 
 
-
-# word_dict = {
-#     "Marvel": [
-#         "The Hulk", "Spiderman", "The Avengers", "Iron Man", "Captain America",
-#         "Thor", "Loki", "Black Widow", "Hawk Eye", "The Red Skull", "Thanos",
-#         "Scarlett Witch", "Vison", "Zemo", "Green Goblin", "The Jackal",
-#         "Fantastic Four", "The Thing", "Silver Surfer",
-#         "Guardians of the Galaxy", "Nick Fury", "Shield", "Vulture",
-#         "Doctor Octopus", "Venom", "The Punisher", "Blade", "Magneto",
-#         "Wolverine", "Cyclops", "Jean Grey", "Beast", "Iron Fist", "Luke Cage",
-#         "Dare Devil", "Scorpion", "Black Panther", "Wakanda",
-#         "Stark Enterprise", "Tony Stark", "Power man", "Abomination",
-#         "Carnage", "I am Groot", "Avengers Assemble", "War Machine",
-#         "Mister Fantastic", "Invisible Woman", "The Human Torch", "Venom",
-#         "Scarlet Witch", "The Fantastic Four", "The Mandarin", "The Wasp",
-#         "Wolverine", "Cyclops", "Jean Grey", "Beast", "Luke Cage",
-#         "Dare Devil", "Sand man", "Beast", "Silver Surfer", "Captain Marvel",
-#         "Venom", "Magneto", "Night Crawler", "Madame Web", "Carnage",
-#         "King pin", "Doctor Octopus", "The Lizard", " She Hulk", "Doctor Doom"
-#     ],
-#     "Artists": [
-#         "Jay Z", "Queen Latifah", "DMX", "Snoop Dogg", "Jadakiss", "J Cole",
-#         "Jermaine Dupri", "Alicia Keys", "John Legend", "Lil Jon", "Lil Kim",
-#         "Diddy", "Tupac", "Lil Wayne", "Beastie Boyz", "Ying Yang Twins",
-#         "Drake", "Cash Money", "Ruff Ryders", "Bad Boy Entertainment",
-#         "Nicki Minaj", "Bryson Tiller", "Torey Lanez", "Summer Walker",
-#         "Usher", "Cardi B", "DMX", "Meg The Stallion", "Chris Brown",
-#         "Rihanna", "The Fugees", "Aretha Franklin", "Jay Rock", "Kanye West",
-#         "Kendrick Lamar", "Kid Cudi", "James Brown", "Michael Jackson", "Drake", "Kendrick Lamar",
-#         "Madonna", "Britney Spears", "Billie Eillish", "Bob Marley", "Jay - Z", "Beyonce", "Destiny Childs"
-#     ],
-#     "NBA": [
-#         "LeBron James", "Carmelo Anthony", "Michael Jordan", "Kobe Bryant",
-#         "Tim Duncan", "Kevin Garnet", "Russell Westbrook", "Pick and Roll",
-#         "New York Knicks", "Los angeles Lakers", " Los Angeles Clippers",
-#         "New Orlean Pelicans", "Charlotte Hornets", "Golden State Warriors",
-#         "Sacremento Kings", "Toronto Raptors", "Stephen Curry", "Kevin Durant",
-#         "Kyrie Irving", "Kawhi Leonard", "Boston Celtics", "Miami Heat",
-#         "Magic Johnson", "Houston Rockets", "Chicago Bulls",
-#         "Oklahoma City Thunder", "Denver Nuggets", "Dallas Mavericks",
-#         "Portland Trail Blazers", "Brooklyn Nets", "Jayson Tatum",
-#         "Jaylen Brown", "Anthony Davis", "Jimmy Butler", "James Harden",
-#         "Jimmy Butler", "Shaquille O'Neal", "Kyrie Irving", "Zion Williamson",
-#         "Ja Morant", "Scottie Pippen", "Jalen Brunson", "Julius Randle", "Stephen A Smith", "Philadelphia Sixers",
-#         "Tracey McGrady", "Vince Carter", "Patrick Ewing", "Yao Ming", "Miami Heat", "Indiana Pacers", "Orlando Magic",
-#         "Doc Rivers"
-#     ],
-#     "Smikle Family": [
-#         "Jermaine", "Denise", "Zaya", "Sharon", "Denise", "Carl Smikle", 
-#         "Naomi Brady", "Ingrid", "Twyla", "Denver", "Jose Lopez", "Roderick", "Jazmine", "Charise", "Dori",
-#         "Madison", "Uncle Dougie", "Samuel", "Kevin", "Franchesca", "Dozer", "Miriam Smith", "Ephraim Brady", "Nerissa Lynch",
-#         "Caleb Smikle", "Uncle Peter", "Enez Brady", "John Brady"
-        
-#     ],
-#     "Zaya's Friends": [
-#         "Zaya", "Bruce", "Leena", "Jose Jr", "Tommy", "Adrian", "Adam", "Leo", "Alex", "Zuri", "Zoe", "Liam",
-#         "Ryan", "Pino", "Charlotte", "Alihan", "Aria", "victoria", "Filippo", "Julian", "Malakai", "Daniel", "Harish"
-#         "Lucas", "Sahana", "Avyan", "Ellie", "Sophia", "Lillian", "Rowan", "Gianna", "Caroline", "Henry", "Drew", "Tyler"
-#         "Claire", "Ayaan", "Ruhaani", "Ilia", "Ethan", "Eleanor", "Liberty", "Mila", "Viveca", "Liana", "Lael", "Nikisha", "David"
-#         "Oona", "Marcella", "Madison", "Rosie", "Anastasia", "Zaire", "George", "Cristina"
-        
-#     ],
-#         "Grandma's Phrases": [
-#             "Don't bother me", "Your head side!", "Those that don't hear, feel", "Get Lost", "What kind of weather is it outside?",
-#             "I am not a Senior Citizen!", "Get the hell of my phone!", "I am not cooking!", "I have a doctor appointment tomorrow", "Pick up my prescription at CVS",
-#             "Keep it Down!", "Never treat somebody better than how they treat you", "Alexa! Play Beres Hammond", "Not enough food in the soup!",
-#             "Its too salty", "I need to use the bathroom"
-        
-#     ],
-#         "Fruits": [
-#             "Apples", "Bananas", "Orange", "Pear", "Pineapple", "Grapes", "Mango", "Watermelon", "Cantalope", "Blackberry", "Blueberry"
-#             "Cherry", "Rasberry", "Cranberry", "Strawberry", "Honeydew", "Peach", "Coconut", "Plum", "Kiwi",
-#             "Key Lime", "Lemon", "Apricot", "Guava", "Tangerine", "Passion Fruit", "Grapefruit", "Pomegranate", "Avocado",
-#             "Dragon Fruit", "Papaya"
-        
-#     ]
-# }
 
 
 
@@ -290,6 +227,8 @@ def selectCategory():
         # btn_x = 500
         # btn_y = 150
         
+        # All buttons on the category page
+        
         marvel_btn = Button("Marvel", GREEN, (300, 50), 120, 50)
         marvel_btn.draw()
         nba_btn = Button("NBA", GREEN, (300, 150), 100, 50)
@@ -304,6 +243,8 @@ def selectCategory():
         grandma_btn.draw()
         fruit_btn = Button("Fruits", GREEN, (50, 50), 100, 50)
         fruit_btn.draw()
+        dc_btn = Button("DC Comics", GREEN, (50, 150), 150, 50)
+        dc_btn.draw()
         
         # btn_list = []
         
@@ -342,6 +283,9 @@ def selectCategory():
             break
         if fruit_btn.checkClicked():
             selected_cat = "Fruits"
+            break
+        if dc_btn.checkClicked():
+            selected_cat = "DC Comics"
             break
             
 
